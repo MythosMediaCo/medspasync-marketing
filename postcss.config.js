3ff62a9d6f15fec4a1a52504f3de29779ccc3021
MythosMediaCo <hagoodjacob@gmail.com>
2025-06-10 16:52:12 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 // medspasync-frontend/postcss.config.js
-// Configured using CommonJS syntax for Node.js v18 and Railway
 module.exports = {
   plugins: {
-    // Tailwind CSS PostCSS plugin
     tailwindcss: {},
-    // Autoprefixer is typically included for vendor prefixes
     autoprefixer: {},
   },
 };
