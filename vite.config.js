79be03445f8dd1da39199c467fa240a31e2ff506
MythosMediaCo <hagoodjacob@gmail.com>
2025-06-10 19:12:12 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/medspasync-frontend/', // <- THIS IS CRUCIAL
+  base: '/', // <-- Fix: serve from root
 });
