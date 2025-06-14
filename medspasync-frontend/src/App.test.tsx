98d4aab7d2a301052db1365232345c4902804d7e
MythosMediaCo <hagoodjacob@gmail.com>
2025-06-14 10:25:33 -0600

    Add transaction uploader component

--- medspasync-frontend/src/App.test.tsx
+++ medspasync-frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders uploader heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/medspa transaction uploader/i);
+  expect(heading).toBeInTheDocument();
 });
